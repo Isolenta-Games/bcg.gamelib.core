448e368b27b5ae289c2746f518e125a1e63fe51e
Artyom Babkin <a.babkin@blackcaviar.games>
2023-06-07 18:00:51 +0300

    [SM-2122] feat: aes encryption utility

---
+++ Runtime/GameLib.Core/Encryption/DummyEncryptor.cs
@@ -0,0 +1,15 @@
+﻿namespace GameLib.Core.Encryption
+{
+	public class DummyEncryptor : IEncryptor
+	{
+		public string Encrypt(string plainText)
+		{
+			return plainText;
+		}
+
+		public string Decrypt(string encryptedText)
+		{
+			return encryptedText;
+		}
+	}
+}
---
+++ Runtime/GameLib.Core/Encryption/IEncryptor.cs
@@ -0,0 +1,8 @@
+﻿namespace GameLib.Core.Encryption
+{
+	public interface IEncryptor
+	{
+		string Encrypt(string plainText);
+		string Decrypt(string encryptedText);
+	}
+}
